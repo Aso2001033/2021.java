01e9383d307f0f869960081f2d94f14187398d7b
Aso2001033 <83046761+Aso2001033@users.noreply.github.com>
2021-07-15 11:01:58 +0900

    Add files via upload

---
+++ Ship.java
@@ -0,0 +1,17 @@
+
+public class Ship{
+    String s1;
+    String s2;
+    String s3;
+    
+    public void dispb(){
+        System.out.println("船1:"s1);
+        System.out.println("船2:"+s2);
+        System.out.println("船3:"+s3);
+    }
+    public void dispa(){
+        System.out.println("船1:"+s1);
+        System.out.println("船2:"+s2);
+        System.out.println("船3:"+s3);
+    }
+}
